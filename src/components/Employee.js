987f5807484f5046f98d73bbd8897b0caaf32831
irfanhathoti <irfanhathoti@gmail.com>
2021-12-28 20:49:03 +0530

    change

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,39 +1,37 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import './Style.css'
 
 export const Employee = () => {
-    const [data,setData] = useState(
+    const [data, setData] = useState(
         {
-            username:"",
-            department:"",
-            rating:""
+            username: "",
+            department: "",
+            rating: ""
 
         }
     )
-    const [record , setRecord] = useState([])
-    const HandleInput = (e) =>
-    {
+    const [record, setRecord] = useState([])
+    const HandleInput = (e) => {
         const name = e.target.name
         const value = e.target.value
 
 
-        setData({ ...data, [name]:value});
+        setData({ ...data, [name]: value });
     }
 
-    const HandleSubmit = (e) =>
-    {
+    const HandleSubmit = (e) => {
         e.preventDefault()
         console.log("hello")
-        const newData = { ...data,id:new Date().getTime().toString()}
+        const newData = { ...data, id: new Date().getTime().toString() }
         console.log(data)
 
 
-        setRecord([ ...record,newData])
+        setRecord([...record, newData])
 
         setData({
-            username:"",
-            department:"",
-            rating:""
+            username: "",
+            department: "",
+            rating: ""
 
         })
         // console.log(record)
@@ -48,42 +46,42 @@ export const Employee = () => {
                 <div className='card'>
                     <label htmlFor='username'>Name  </label>
                     <br />
-                    <input type='text'  value={data.username} onChange={HandleInput} name='username' id='username' />
+                    <input type='text' autoComplete='off' value={data.username} onChange={HandleInput} name='username' id='username' />
                 </div>
 
                 <div className='card'>
                     <label htmlFor='department'>Department  </label>
                     <br />
-                    <input type='text'  value={data.department} onChange={HandleInput} name='department' id='department' />
+                    <input type='text'autoComplete='off' value={data.department} onChange={HandleInput} name='department' id='department' />
                 </div>
 
                 <div className='card'>
                     <label htmlFor='rate'>Rating  </label>
                     <br />
-                    <input type='text' value={data.rate} onChange={HandleInput}  name='rating' id='rating' />
+                    <input type="number" autoComplete='off' value={data.rate} onChange={HandleInput} name='rating' id='rating' />
                 </div>
                 <button type="submit">Submit</button>
 
             </form>
-            <div>
+            <div className='parent'>
                 {
                     record.map((curElem) => {
-                        const {id, username, department , rating} = curElem;
-                        return(
-                            <div className='parent'>
-                                <span>Name : {username}</span>
-                                <span>Department : {department} </span>
-                                <span>Rating : {rating} </span>
+                        const { id, username, department, rating } = curElem;
+                        return (
+                            <div className='child'>
+                                <span>Name : {username} | </span>
+                                <span>Department : {department} | </span>
+                                <span>Rating : {rating}</span>
                             </div>
                         )
-                        console.log(curElem)
+                       
                     })
-                    
+
 
                 }
             </div>
 
-            
+
 
 
         </>
